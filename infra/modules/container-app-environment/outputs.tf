48712f934a71e0d06fa1c81c0154bce5d4e99501
Patryk Jabłoński <pmjablonski@student.wsb-nlu.edu.pl>
2026-07-02 20:33:58 +0200

    tf: fix container-app output

--- infra/modules/container-app-environment/outputs.tf
+++ infra/modules/container-app-environment/outputs.tf
@@ -1,9 +1,9 @@
 output "environment_id" {
   description = "ID of the container app environment"
-  value       = azurerm_container_app_environment.container_app_environment_name
+  value       = azurerm_container_app_environment.main.id
 }
 
 output "infrastructure_subnet_id" {
   description = "ID of the subnet used for the container app environment"
-  value       = azurerm_container_app_environment.infrastructure_subnet_id
+  value       = azurerm_container_app_environment.main.infrastructure_subnet_id
 }
